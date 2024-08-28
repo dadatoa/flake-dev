b8fac6e133935e63b9bf7ade7085a37628697f92
Dadatoa <f0a202@outlook.com>
2024-08-28 15:21:10 +0700

    launch tmux when start nix develop

--- flake.nix
+++ flake.nix
@@ -10,11 +10,23 @@
       systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ glab gh starship fish tmux ];
+          nativeBuildInputs = with pkgs; [ glab gh starship fish tmux bun ];
           shellHook = ''
           export STARSHIP_CONFIG=$PWD/.config/starship.toml
           eval "$(starship init bash)"
-          # fish
+          
+          # Start tmux session
+          SESSION_NAME="$(basename $PWD)"
+          # Check if the session already exists
+          if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+            echo "Session $SESSION_NAME already exists. Attaching to it."
+            tmux attach-session -t $SESSION_NAME
+          else
+          # Create a new session and name it
+            tmux new-session -d -s $SESSION_NAME
+            # Attach to the created session
+            tmux attach-session -t $SESSION_NAME
+          fi
           '';
         };
 
